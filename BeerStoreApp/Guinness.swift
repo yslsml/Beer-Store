2d968705ffbd38fe48c7c3abe14c02920cc8f6c2
Milana Antonova <mmf.antonovam@gmail.com>
2023-01-04 12:31:40 +0300

    add 3 beer classes

---
+++ BeerStoreApp/Guinness.swift
@@ -0,0 +1,22 @@
+//
+//  Guinness.swift
+//  BeerStoreApp
+//
+//  Created by Milana Antonova on 4.01.23.
+//
+
+import Foundation
+
+class Guinness {
+    
+    var name: String
+    var price: Double
+    var remainingVolume: Int
+    
+    init(name: String, price: Double, remainingVolume: Int) {
+        self.name = name
+        self.price = price
+        self.remainingVolume = remainingVolume
+    }
+    
+}

37755801f10fa88be1e012881f3ef68ef547496f
Milana Antonova <mmf.antonovam@gmail.com>
2023-01-04 19:31:16 +0300

    add sell beer functionality

--- BeerStoreApp/Manager.swift
+++ BeerStoreApp/Manager.swift
@@ -13,10 +13,37 @@ class Manager {
     
     private init() {}
     
+    var totalIncome = 0.0
+    
     let beers: [Beer] = [
             Beer(name: "Heineken", price: 2.5, remainingVolume: 100, sellQuantity: 0),
             Beer(name: "Carlsberg", price: 2.9, remainingVolume: 100, sellQuantity: 0),
             Beer(name: "Giunness", price: 3.1, remainingVolume: 100, sellQuantity: 0)
         ]
 
+    func sellBeer(_ beer: Beer) {
+        beer.remainingVolume -= beer.sellQuantity
+        beer.sellQuantity = 0
+    }
+    
+    func sell() {
+        var totalSum = getTotalSumOfShopping()
+        for beer in beers {
+            sellBeer(beer)
+        }
+        totalIncome += totalSum
+    }
+    
+    func getTotalSumForBeer(_ beer: Beer) -> Double {
+        return beer.price * Double(beer.sellQuantity)
+    }
+    
+    func getTotalSumOfShopping() -> Double {
+        var totalSum = 0.0
+        for beer in beers {
+            totalSum += getTotalSumForBeer(beer)
+        }
+        return totalSum
+    }
+    
 }
--- BeerStoreApp/ViewController.swift
+++ BeerStoreApp/ViewController.swift
@@ -34,42 +34,60 @@ class ViewController: UIViewController {
 
     //MARK: IBActions
     @IBAction func addFirstBeerButtonPressed(_ sender: UIButton) {
+        messageLabel.text = ""
         if (Manager.shared.beers[0].sellQuantity < Manager.shared.beers[0].remainingVolume) {
             Manager.shared.beers[0].sellQuantity += 1
             firstBeerQuantityLabel.text = String(Manager.shared.beers[0].sellQuantity)
         }
     }
     @IBAction func addSecondBeerButtonPressed(_ sender: UIButton) {
+        messageLabel.text = ""
         if (Manager.shared.beers[1].sellQuantity < Manager.shared.beers[1].remainingVolume) {
             Manager.shared.beers[1].sellQuantity += 1
             secondBeerQuantityLabel.text = String(Manager.shared.beers[1].sellQuantity)
         }
     }
     @IBAction func addThirdBeerButtonPressed(_ sender: UIButton) {
+        messageLabel.text = ""
         if (Manager.shared.beers[2].sellQuantity < Manager.shared.beers[2].remainingVolume) {
             Manager.shared.beers[2].sellQuantity += 1
             thirdBeerQuantityLabel.text = String(Manager.shared.beers[2].sellQuantity)
         }
     }
     @IBAction func subtractFirstBeerButtonPressed(_ sender: UIButton) {
+        messageLabel.text = ""
         if (Manager.shared.beers[0].sellQuantity > 0) {
             Manager.shared.beers[0].sellQuantity -= 1
             firstBeerQuantityLabel.text = String(Manager.shared.beers[0].sellQuantity)
         }
     }
     @IBAction func subtractSecondBeerButtonPressed(_ sender: UIButton) {
+        messageLabel.text = ""
         if (Manager.shared.beers[1].sellQuantity > 0) {
             Manager.shared.beers[1].sellQuantity -= 1
             secondBeerQuantityLabel.text = String(Manager.shared.beers[1].sellQuantity)
         }
     }
     @IBAction func subtractThirdBeerButtonPressed(_ sender: UIButton) {
+        messageLabel.text = ""
         if (Manager.shared.beers[2].sellQuantity > 0) {
             Manager.shared.beers[2].sellQuantity -= 1
             thirdBeerQuantityLabel.text = String(Manager.shared.beers[2].sellQuantity)
         }
     }
     @IBAction func sellButtonPressed(_ sender: UIButton) {
+        var firstBeerTotalSum = Manager.shared.getTotalSumForBeer(Manager.shared.beers[0])
+        var secondBeerTotalSum = Manager.shared.getTotalSumForBeer(Manager.shared.beers[1])
+        var thirdBeerTotalSum = Manager.shared.getTotalSumForBeer(Manager.shared.beers[2])
+        var totalSum = Manager.shared.getTotalSumOfShopping()
+        
+        messageLabel.text = "\(Manager.shared.beers[0].name): \(Manager.shared.beers[0].price) * \(Manager.shared.beers[0].sellQuantity) = \(firstBeerTotalSum)$ \n \(Manager.shared.beers[1].name): \(Manager.shared.beers[1].price) * \(Manager.shared.beers[1].sellQuantity) = \(secondBeerTotalSum)$ \n\(Manager.shared.beers[2].name): \(Manager.shared.beers[2].price) * \(Manager.shared.beers[2].sellQuantity) = \(thirdBeerTotalSum)$ \nTotal sum: \(totalSum)$"
+        
+        Manager.shared.sell()
+        
+        firstBeerQuantityLabel.text = "0"
+        secondBeerQuantityLabel.text = "0"
+        thirdBeerQuantityLabel.text = "0"
     }
     @IBAction func startDayButtonPressed(_ sender: UIButton) {
     }
